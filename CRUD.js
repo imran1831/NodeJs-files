f71875abb83306b1d836b8fcbef5271db6fd7f3f
imran1831 <imran807428@gmail.com>
2025-12-26 11:25:05 +0530

    Added new NXT file into node.js files

---
+++ CRUD.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+app.use(express.json());
+const path = require("path");
+const dbPath = path.join(__dirname, "goodreads.db");
+let db = null;
+const initializeDBAndServer = async () => {
+    try {
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database,
+        });
+        app.listen((3000), () => console.log("Server is running at http://localhost:3000"));
+    }
+    catch (e) {
+        console.log(`DB Error:${e.message}`);
+        process.exit(1);
+    }
+};
+initializeDBAndServer();
+
+app.get('/books/', async (req, res) => {
+    const getBooksQuery = `
+        SELECT
+         *
+        FROM
+        book
+        ORDER
+        BY
+        book_id;
+    `;
+    const booksArray = await db.all(getBooksQuery);
+    res.send(booksArray);
+});
+
+// Add Book API
+app.post("/books/", async (req, res) => {
+    const bookDetails = req.body;
+    const {
+        title,
+        authorId,
+        rating,
+        ratingCount,
+        reviewCount,
+        description,
+        pages,
+        dateOfPublication,
+        editionLanguage,
+        price,
+        onlineStores,
+    } = bookDetails;
+    const addBookQuery = `
+INSERT INTO
+book (title,author_id,rating,rating_count,review_count,description,pages,date_of_publication,edition_language,price,online_stores)
+VALUES
+(
+    '${title}',
+    ${authorId},
+    ${rating},
+    ${ratingCount},
+    ${reviewCount},
+    '${description}',
+    ${pages},
+    '${dateOfPublication}',
+    '${editionLanguage}',
+    ${price},
+    '${onlineStores}'
+);
+`;
+
+    const dbResponse = await db.run(addBookQuery);
+    const bookId = dbResponse.lastID;
+    res.send({ bookId: bookId });
+});
---
+++ httpmethod.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+// Home route
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+// Date route
+app.get("/date", (req, res) => {
+    const date = new Date();
+    res.send(`Today's date is ${date}`);
+});
+
+// HTML page route
+app.get("/page", (req, res) => {
+    res.sendFile("page.html", { root: __dirname });
+});
+
+// Start server
+app.listen(4000, () => {
+    console.log("Server running on http://localhost:4000");
+});
